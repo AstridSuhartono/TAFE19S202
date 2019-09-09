47c8fda7be15677b202540d0f7111d3b4f0eb2e4
HyejinLee <Hyejin.Lee@student.tafesa.edu.au>
2019-09-09 18:57:59 +0930

    noooo

---
+++ InstaRichie/Models/Personal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    public class Personal
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonalID { get; set; }
+        [NotNull]
+        public string FirstName { get; set; }
+        [NotNull]
+        public string LastName { get; set; }
+        [NotNull]
+        public DateTime DOB { get; set; }
+        [Unique, NotNull]
+        public string Email { get; set; }
+        [Unique, NotNull]
+        public string MobileNumber { get; set; }
+    }
+}
