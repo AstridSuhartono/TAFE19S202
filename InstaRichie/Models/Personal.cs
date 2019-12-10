fb2b12e90cacf8ff64fddf0c90176a3ff62901ea
Hyejin Lee <hyejinlee@student.tafesa.edu.au>
2019-12-10 17:51:47 +1030

    hyejin branch for personal info

---
+++ InstaRichie/Models/Personal.cs
@@ -0,0 +1,25 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class Personal
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonalID { get; set; }
+        [NotNull]
+        public string FirstName { get; set; }
+        [NotNull]
+        public string LastName { get; set; }
+        [NotNull]
+        public DateTime DOB { get; set; }
+        [Unique, NotNull]
+        public string Email { get; set; }
+        [Unique, NotNull]
+        public string MobileNumber { get; set; }
+    }
+}
